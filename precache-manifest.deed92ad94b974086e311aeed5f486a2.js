397116d30a1c41102e570f5623607b95e524f841
faizan-appdirect <faizan.shaikh@appdirect.com>
2019-11-13 23:42:14 +0530

    Updates

---
+++ precache-manifest.deed92ad94b974086e311aeed5f486a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fcf6ff274ce3587899f0e035156c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2661557528b328aee9d2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a96a59449d7e983bb41c",
+    "url": "/static/js/2.011d91f6.chunk.js"
+  },
+  {
+    "revision": "2661557528b328aee9d2",
+    "url": "/static/js/main.c3b107b2.chunk.js"
+  },
+  {
+    "revision": "f98afa02526a6d707983",
+    "url": "/static/js/runtime-main.3ea4d90c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
